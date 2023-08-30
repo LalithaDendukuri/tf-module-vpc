3be2de4e394ae6cf3c524dc9e1627415cf678ef3
Lalitha <lalitha.d339@gmail.com>
2023-08-30 05:41:36 +0530

    terraform variables

--- locals.tf
+++ locals.tf
@@ -1,13 +1,13 @@
 locals {
   # Subnets
-  public_subnet_ids= lookup(lookup(module.subnets,"public",null ),"subnet_ids",null)
-  app_subnet_ids= lookup(lookup(module.subnets,"app",null ),"subnet_ids",null)
-  db_subnet_ids= lookup(lookup(module.subnets,"db",null ),"subnet_ids",null)
+  public_subnet_ids= [for k, v in lookup(lookup(module.subnets,"public",null ),"subnet_ids",null) : v.id]
+  app_subnet_ids= [for k, v in lookup(lookup(module.subnets,"app",null ),"subnet_ids",null) : v.id]
+  db_subnet_ids= [for k, v in lookup(lookup(module.subnets,"db",null ),"subnet_ids",null) : v.id]
   private_subnet_ids=  concat(local.app_subnet_ids,local.db_subnet_ids)
 
   #RT
-  public_rt_ids= lookup(lookup(module.subnets,"public",null ),"aws_route_table_ids",null)
-  app_rt_ids= lookup(lookup(module.subnets,"app",null ),"aws_route_table_ids",null)
-  db_rt_ids= lookup(lookup(module.subnets,"db",null ),"aws_route_table_ids",null)
+  public_rt_ids= [for k, v in lookup(lookup(module.subnets,"public",null ),"aws_route_table_ids",null) : v.id]
+  app_rt_ids= [for k, v in lookup(lookup(module.subnets,"app",null ),"aws_route_table_ids",null) : v.id]
+  db_rt_ids= [for k, v in lookup(lookup(module.subnets,"db",null ),"aws_route_table_ids",null) : v.id]
   private_rt_ids=  concat(local.app_rt_ids,local.db_rt_ids)
 }
